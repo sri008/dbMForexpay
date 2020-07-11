257d3ea86f0417bbee6dfec25f7d8c08ebcb761e
KarriSpandana <66367375+KarriSpandana@users.noreply.github.com>
2020-07-11 10:49:36 +0530

    Update V2__Add_people.sql

--- src/main/resources/db/migration/V2__Add_people.sql
+++ src/main/resources/db/migration/V2__Add_people.sql
@@ -1,2 +1,7 @@
 
-INSERT INTO person (id) VALUES(2);
+INSERT INTO team (id,name) VALUES(1, 'Spandana');
+INSERT INTO team (id,name) VALUES(2, 'Baikuntha');
+INSERT INTO team (id,name) VALUES(3, 'Eeresh');
+INSERT INTO team (id,name) VALUES(4, 'Srikant');
+INSERT INTO team (id,name) VALUES(5, 'Akash');
+                                  
