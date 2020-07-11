d13f6aca56802daa48ce98c60f3bd27ac1c939c6
KarriSpandana <66367375+KarriSpandana@users.noreply.github.com>
2020-07-11 13:27:17 +0530

    Delete V2__Add_people.sql

--- src/main/resources/db/migration/V2__Add_people.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO team (id,name) VALUES(1, 'Spandana');
-INSERT INTO team (id,name) VALUES(2, 'Baikuntha');
-INSERT INTO team (id,name) VALUES(3, 'Eeresh');
-INSERT INTO team (id,name) VALUES(4, 'Srikant');
-
-                                  
